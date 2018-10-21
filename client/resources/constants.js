2941d816b74069e86e36df1abd95c300f91944c5
Jan Jůna <jan.juna-ext@commercetools.de>
2018-10-21 14:55:01 +0200

    Add menu component

---
+++ client/resources/constants.js
@@ -0,0 +1,29 @@
+export const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
+
+export const vxFuturesMonthlyCode = {
+  January:	'F',
+  February:	'G',
+  March:	'H',
+  April:	'J',
+  May:	'K',
+  June:	'M',
+  July:	'N',
+  August:	'Q',
+  September:	'U',
+  October:	'V',
+  November:	'X',
+  December:	'Z'
+}
---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Header></Header>
+    <Sidebar></Sidebar>
+
+    <div class="content-wrapper">
+      <!--<BreadCrumbs></BreadCrumbs>-->
+
+      <!--<section class="content">-->
+        <!--<router-view/>-->
+      <!--</section>-->
+    </div>
+
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+
+export default {
+  name: 'VolatilityWatcher',
+  components: {
+    Header,
+    Sidebar,
+    Footer,
+  },
+};
+</script>
